0b15ee2c388b06cbac724d87450cc166b7378abd
josh <112547441+joshvirtuaali@users.noreply.github.com>
2025-11-18 13:00:12 +0200

    Muokattu linkki tuen sähköpostiin

--- fuel/application/views/puljut/index.php
+++ fuel/application/views/puljut/index.php
@@ -2,4 +2,4 @@
 
 <p>VRL:n alaisuudessa toimii useita erilaisia alayhdistyksiä, esimerkiksi kilpailujaoksia ja näiden laatuarvosteluja, muita laatuarvosteluja, kantakirjoja ja rotuyhdistyksiä. </p>
 
-<p>Alasivuilta voit selata yhdistyksiä ja tapahtuma-arkistoa. Mikäli haluat oman yhdistyksesi VRL:n alaisuuteen, ole yhteydessä <a href="<?php echo site_url('tuki') ?>" title="Ota yhteyttä tukeen">tukeen</a>!</p>
+<p>Alasivuilta voit selata yhdistyksiä ja tapahtuma-arkistoa. Mikäli haluat oman yhdistyksesi VRL:n alaisuuteen, ole yhteydessä <a href="mailto:vrlvirallinen@gmail.com" title="Ota yhteyttä jaosvastaaviin">jaosvastaaviin</a>!</p>

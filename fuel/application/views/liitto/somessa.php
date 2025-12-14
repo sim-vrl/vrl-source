cd3c034609083ff88d84fc631b8d6f4e16a584b2
sabosr <sannaro90@gmail.com>
2025-12-14 11:36:56 +0200

    Muutettu instalinkki

--- fuel/application/views/liitto/somessa.php
+++ fuel/application/views/liitto/somessa.php
@@ -1,7 +1,6 @@
 <h1>VRL sosiaalisessa mediassa</h1>
 
-<p>Virtuaalisen Ratsastajainliiton viralliset sosiaalisen median kanavat ovat Facebook
-(<a href="https://www.facebook.com/virtuaalihevoset/">https://www.facebook.com/virtuaalihevoset/</a>) ja Instagram
-(<a href="https://www.instagram.com/virtuaalihevoset/">https://www.instagram.com/virtuaalihevoset/</a>).</p>
+<p>Virtuaalisen Ratsastajainliiton virallinen sosiaalisen median kanava on Instagram
+(<a href="https://www.instagram.com/vrlvirallinen/">https://www.instagram.com/vrlvirallinen/</a>).</p>
 
-<p>Sosiaalisen median kanavien tarkoituksena on lisätä tietoa virtuaalihevosista muun muassa erinäisin esittelyin, tapahtumatempauksin ja ajankohtaisista asioista informoiden. Tuotamme erityisesti Instagramiin monipuolista sisältöä, jonka sisältöön pääsee vaikuttamaan myös harrastajat. Jos sinulla on juttuvinkki esimerkiksi ajankohtaisesta tapahtumasta tai vaikka rodun tai lajin esittelystä, olethan yhteydessä sosiaalisen median tuottajiin joko sähköpostitse virtuaalinenratsastajainliitto@gmail.com tai Instagramin chatviestitoiminnolla. </p>
+<p>Sosiaalisen median kanavien tarkoituksena on lisätä tietoa virtuaalihevosista muun muassa erinäisin esittelyin, tapahtumatempauksin ja ajankohtaisista asioista informoiden. Tuotamme erityisesti Instagramiin monipuolista sisältöä, jonka sisältöön pääsee vaikuttamaan myös harrastajat. Jos sinulla on juttuvinkki esimerkiksi ajankohtaisesta tapahtumasta tai vaikka rodun tai lajin esittelystä, olethan yhteydessä sosiaalisen median tuottajiin joko sähköpostitse vrlvirallinen@gmail.com tai Instagramin chatviestitoiminnolla. </p>

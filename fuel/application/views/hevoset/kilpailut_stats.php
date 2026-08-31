3b8a80bcb06aaf72815b27f3cb1702c0740668a9
artisokka <janika.l.ranta@gmail.com>
2026-08-31 22:54:51 +0300

    Fix: Issue #22 - TYH-statistiikka puuttuu profiilista #WIP

--- fuel/application/views/hevoset/kilpailut_stats.php
+++ fuel/application/views/hevoset/kilpailut_stats.php
@@ -2,93 +2,87 @@
 
 <?php
 
-echo "<h3>Porrastetut kilpailut</h2>";
+echo "<h3>Porrastetut kilpailut</h3>";
 tableHead();
-    
-foreach ($kisatiedot as $jaos=>$info){
-    if($jaokset[$jaos]['s_salli_porrastetut'] == 0){
+
+foreach ($jaokset as $jaos_id => $jaos) {
+    // Näytetään vain porrastetut kilpailujaokset
+    if (empty($jaos['s_salli_porrastetut'])) {
         continue;
     }
-    
+
+    $info = $kisatiedot[$jaos_id] ?? array();
+    $voi = $info['porr_voi'] ?? 0;
+    $sij = $info['porr_sij'] ?? 0;
+    $os  = $info['porr_os'] ?? 0;
 
     echo "<tr>";
-    echo "<td><b>". $jaokset[$jaos]['lyhenne']. "</b></td>";
-    echo "<td>".$info['porr_voi']."</td>";
-    echo "<td>".$info['porr_sij']."</td>";
-    echo "<td>".$info['porr_os']."</td>";
-        echo "<td>".sijpros($info['porr_voi'], $info['porr_sij'], $info['porr_os']) ."%</td>";
-
+    echo "<td><b>" . htmlspecialchars($jaos['lyhenne']) . "</b></td>";
+    echo "<td>" . $voi . "</td>";
+    echo "<td>" . $sij . "</td>";
+    echo "<td>" . $os . "</td>";
+    echo "<td>" . sijpros($voi, $sij, $os) . "%</td>";
     echo "</tr>";
-    
 }
 
 tableEnd();
 
 
 echo "<h3>Perinteiset kilpailut</h3>";
-    
-  
 tableHead();
-    
-foreach ($kisatiedot as $jaos=>$info){
-    echo "<tr>";
-    echo "<td><b>". $jaokset[$jaos]['lyhenne']. "</b></td>";
-    echo "<td>".$info['voi']."</td>";
-    echo "<td>".$info['sij']."</td>";
-    echo "<td>".$info['os']."</td>";
-        echo "<td>".sijpros($info['voi'], $info['sij'], $info['os']) ."%</td>";
 
+foreach ($jaokset as $jaos_id => $jaos) {
+    // Ohitetaan näyttelyjaokset perinteisistä urheilukilpailuista
+    if (!empty($jaos['nayttelyt'])) {
+        continue;
+    }
+
+    $info = $kisatiedot[$jaos_id] ?? array();
+    $voi = $info['voi'] ?? 0;
+    $sij = $info['sij'] ?? 0;
+    $os  = $info['os'] ?? 0;
+
+    echo "<tr>";
+    echo "<td><b>" . htmlspecialchars($jaos['lyhenne']) . "</b></td>";
+    echo "<td>" . $voi . "</td>";
+    echo "<td>" . $sij . "</td>";
+    echo "<td>" . $os . "</td>";
+    echo "<td>" . sijpros($voi, $sij, $os) . "%</td>";
     echo "</tr>";
-    
 }
 
 tableEnd();
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    function sijPros($voi, $sij, $os){
-        $voi = intval(round($voi));
-        $sij = intval(round($sij));
-        $os = intval(round($os));
-        
-        if ($os === 0){
-            return 0;
-        }
-        else {
-            $sijpros = ($voi + $sij)/$os;
-            $sijpros = $sijpros * 100;
-            $sijpros = round($sijpros);
-            return $sijpros;
-        }
-    }
-    
-    
-    function tableHead(){
-          echo '<table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Jaos</th>
-            <th scope="col">Voitot</th>
-            <th scope="col">Muut sijoitukset</th>
-            <th scope="col">Osallistumiset</th>        
-            <th scope="col">Sijoitusprosentti</th>
-          </tr>
-        </thead>';
-    echo "<tbody>";
-    }
 
-    
-    function tableEnd(){
-        echo "</tbody>";
-    echo "</table>";
-    
+
+function sijpros($voi, $sij, $os) {
+    $voi = intval(round($voi));
+    $sij = intval(round($sij));
+    $os  = intval(round($os));
+
+    if ($os === 0) {
+        return 0;
+    } else {
+        $sijpros = (($voi + $sij) / $os) * 100;
+        return round($sijpros);
     }
-    
-    
-    ?>
+}
+
+function tableHead() {
+    echo '<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Jaos</th>
+        <th scope="col">Voitot</th>
+        <th scope="col">Muut sijoitukset</th>
+        <th scope="col">Osallistumiset</th>        
+        <th scope="col">Sijoitusprosentti</th>
+      </tr>
+    </thead>
+    <tbody>';
+}
+
+function tableEnd() {
+    echo "</tbody>
+    </table>";
+}
+?>
